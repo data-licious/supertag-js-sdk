98ce72c857592fc7478fb0e73bc787d588590dc5
Hao Chang <yngwie.chang@gmail.com>
2015-09-03 14:36:35 +1000

    Initialized gulp build system.

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    plumber = require('gulp-plumber'),
+    del = require('del'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+
+gulp.task('js', function() {
+    gulp.src('src/**/*.js')
+        .pipe(plumber())
+        .pipe(concat('st.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/'))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.js', ['js']);
+});
+
+gulp.task('default', ['js', 'browser-sync', 'watch']);
