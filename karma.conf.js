4a98fdbaa7aae9797f0daabf8ccad1549ee241df
Hao Chang <yngwie.chang@gmail.com>
2015-09-23 14:16:26 +1000

    Updated Karma conf to use src files other than dist ones.

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,25 @@
 module.exports = function(config) {
-  config.set({
-    basePath: './',
-    frameworks: ['jasmine'],
-    files: [
-      'bower_components/jquery/dist/jquery.min.js',
-      'test/unit/**/*.js'
-    ],
-    exclude: [],
-    preprocessors: {},
-    reporters: ['dots'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['PhantomJS2'],
-    plugins: [
-      'karma-phantomjs2-launcher',
-      'karma-jasmine'
-    ],
-    singleRun: false
-  });
+    config.set({
+        basePath: './',
+        frameworks: ['jasmine'],
+        files: [
+            'bower_components/jquery/dist/jquery.min.js',
+            'src/st.js',
+            'src/**/*.js',
+            'test/unit/**/*.js'
+        ],
+        exclude: [],
+        preprocessors: {},
+        reporters: ['dots'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['PhantomJS2'],
+        plugins: [
+            'karma-phantomjs2-launcher',
+            'karma-jasmine'
+        ],
+        singleRun: false
+    });
 };
