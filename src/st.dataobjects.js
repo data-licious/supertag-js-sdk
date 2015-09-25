99f7ddc7ab91133829483bb1f7183e2bc397a397
Artem Kolesnikov <tyomo4ka@gmail.com>
2015-09-25 18:15:19 +1000

    Added dataobjects module.

---
+++ src/st.dataobjects.js
@@ -0,0 +1,16 @@
+(function(window) {
+    'use strict';
+
+    var StSDK = window.StSDK;
+
+    /**
+     * Gets all data objects available in the specific project
+     *
+     * @param {Number} projectId ID of the project
+     *
+     * @returns {jqXHR}
+     */
+    StSDK.prototype.getProjectDataObjects = function(projectId) {
+        return this.get('/projects/' + projectId + '/data-objects');
+    };
+}(window));
