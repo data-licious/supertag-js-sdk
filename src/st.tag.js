95b84371cc98290a9fd03611e155ab75c1212c8d
Hao Chang <yngwie.chang@gmail.com>
2015-09-28 14:11:54 +1000

    Added method "installTag".

--- src/st.tag.js
+++ src/st.tag.js
@@ -15,4 +15,21 @@
 
         return this.get('projects/' + id + '/tags');
     };
+
+    /**
+     * Installs a tag into a project
+     *
+     * @param {Number} projectId The project ID
+     * @param {Number} tagTemplateId The tag template ID
+     * @param {Object} data The data of the tag
+     *
+     * @returns {jqXHR}
+     */
+    StSDK.prototype.installTag = function(projectId, tagTemplateId, data) {
+        this.validateInt('Project ID', projectId);
+        this.validateInt('Tag template ID', tagTemplateId);
+        this.validatePlainObj('Tag data', data);
+
+        return this.post('projects/' + projectId +'/tags/template/' + tagTemplateId, null, data);
+    };
 }(window, document, jQuery));
