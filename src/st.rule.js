b881cbab102b88cd39c0f3592cbbe556633d5fcb
w3c.html <w3c.html@gmail.com>
2015-10-14 16:56:46 +0300

    Updated dist and docs, rule operator human readable labels added

--- src/st.rule.js
+++ src/st.rule.js
@@ -34,4 +34,41 @@
         return "";
     };
 
+
+    /**
+     * Returns rule human readable label for rule operator
+     *
+     * @param {Object} data rule object
+     *
+     * @returns {String}
+     */
+    StSDK.prototype.getRuleOperatorLabel = function(operatorName) {
+        switch (operatorName) {
+            case "contains":
+                return "Contains";
+            case "ends_with":
+                return "Ends with";
+            case "equal":
+                return "Equal";
+            case "greater_than":
+                return "Greater than";
+            case "less_than":
+                return "Less than";
+            case "logical_and":
+                return "Logical and";
+            case "logical_or":
+                return "Logical or";
+            case "not_contains":
+                return "Not contains";
+            case "not_equal":
+                return "Not equal";
+            case "regex_match":
+                return "Regex match";
+            case "starts_with":
+                return "Starts with";
+        }
+
+        return "";
+    };
+
 }(window));
