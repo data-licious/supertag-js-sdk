cb426ff3d8c1947055f6d961a747c7546efd240c
w3c.html <w3c.html@gmail.com>
2015-10-26 21:27:21 +0300

    add variable list for project call, Updated dist and docs

--- src/st.rule.js
+++ src/st.rule.js
@@ -3,6 +3,19 @@
     var StSDK = window.StSDK;
 
     /**
+     * Gets all dataObjects available in the given project
+     *
+     * @param {Number} id ID of the project
+     *
+     * @returns {jqXHR}
+     */
+    StSDK.prototype.getDataObjectList = function(id) {
+        StSDK.validateInt('Project ID', id);
+
+        return this.get('projects/' + id + '/data-object-list');
+    };
+
+    /**
      * Gets all rules available in the given project
      *
      * @param {Number} id ID of the project
@@ -44,8 +57,8 @@
      *
      * @returns {String}
      */
-    StSDK.prototype.getRuleObjectTypeLabel = function(data) {
-        switch (data.type) {
+    StSDK.prototype.getRuleTypeLabel = function(data) {
+        switch (data) {
             case "business_rule":
                 return "Business rule";
             case "event":
@@ -104,6 +117,44 @@
         }
 
         return "";
+    };
+
+    /**
+     * Returns rule type list
+     *
+     * @returns {Array}
+     */
+    StSDK.prototype.getRuleTypeList = function() {
+        return ['business_rule', 'event'];
+    };
+
+    /**
+     * Returns rule condition logical operator list
+     *
+     * @returns {Array}
+     */
+    StSDK.prototype.getConditionLogicalOperatorList = function() {
+        return ['logical_and', 'logical_or'];
+    };
+
+
+    /**
+     * Returns rule condition operator list
+     *
+     * @returns {Array}
+     */
+    StSDK.prototype.getConditionOperatorList = function() {
+        return [
+                "contains",
+                "ends_with",
+                "equal",
+                "greater_than",
+                "less_than",
+                "not_contains",
+                "not_equal",
+                "regex_match",
+                "starts_with"
+            ];
     };
 
 }(window));
