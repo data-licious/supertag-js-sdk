6dad630a320ca3fc4b39dfc500485bfdec096621
Hao Chang <yngwie.chang@gmail.com>
2015-11-06 13:57:23 +1100

    Added method "getProjectApplications".

--- src/st.project.js
+++ src/st.project.js
@@ -4,6 +4,21 @@
     var StSDK = window.StSDK;
 
     /**
+     * @constant {String} PROJECT_APP_FORMAT_FLAT
+     */
+    StSDK.PROJECT_APP_FORMAT_FLAT = 'flat';
+
+    /**
+     * @constant {String} PROJECT_APP_FORMAT_GROUPED_WITH_PLATFORM
+     */
+    StSDK.PROJECT_APP_FORMAT_GROUPED_WITH_PLATFORM = 'grouped_with_platform';
+
+    /**
+     * @constant {String} PROJECT_APP_FORMAT_STANDARD_WITH_PLATFORM
+     */
+    StSDK.PROJECT_APP_FORMAT_STANDARD_WITH_PLATFORM = 'standard_with_platform';
+
+    /**
      * Gets companies and projects relevant to the specific user
      *
      * @param {Number} limit The limit of search
@@ -35,7 +50,7 @@
 
 
     /**
-     * Gets Project deployment history data based on Project Id
+     * Gets the deployment history for a given project
      *
      * @param {Number} id The project ID
      *
@@ -43,12 +58,12 @@
      */
     StSDK.prototype.getProjectDeployHistory = function(id) {
         StSDK.validateInt('Project ID', id);
-        return this.get('projects/' + id + '/deployments');        
+        return this.get('projects/' + id + '/deployments');
     };
 
 
     /**
-     * Retrieves Project Release notes prior to deployement (requires Admin role)
+     * Gets the release notes for a given project
      *
      * @param {Number} id The project ID
      *
@@ -57,5 +72,58 @@
     StSDK.prototype.getProjectReleaseNotes = function(id) {
         StSDK.validateInt('Project ID', id);
         return this.get('projects/' + id + '/release-notes');
+    };
+
+    /**
+     * Gets the applications installed in a given project
+     *
+     * @param {Number} id The project ID
+     * @param {String} format The format of the response, options are `'flat'`, `'grouped_with_platform'` or `'standard_with_platform'`
+     *
+     * @returns {jqXHR}
+     */
+    StSDK.prototype.getProjectApplications = function(id, format) {
+        var format = format || StSDK.PROJECT_APP_FORMAT_FLAT;
+        StSDK.validateInt('Project ID', id);
+        StSDK.validateOpts('Format', format, [
+            StSDK.PROJECT_APP_FORMAT_FLAT,
+            StSDK.PROJECT_APP_FORMAT_GROUPED_WITH_PLATFORM,
+            StSDK.PROJECT_APP_FORMAT_STANDARD_WITH_PLATFORM
+        ]);
+
+        return this.get('projects/' + id + '/apps', null, {
+            dataFilter: function(data, type) {
+                if (StSDK.PROJECT_APP_FORMAT_FLAT === format || 'json' !== type) {
+                    return data;
+                }
+
+                var grouped = {},
+                    standard = [],
+                    apps = StSDK.jsonDecode(data);
+
+                $.each(apps, function(k, app) {
+                    var platform = app.platform;
+
+                    if (!(platform in grouped)) {
+                        grouped[platform] = [app]
+                    } else {
+                        grouped[platform].push(app);
+                    }
+                });
+
+                if (StSDK.PROJECT_APP_FORMAT_GROUPED_WITH_PLATFORM === format) {
+                    return StSDK.jsonEncode(grouped);
+                }
+
+                $.each(grouped, function(platformName, apps) {
+                    standard.push({
+                        platform: platformName,
+                        applications: apps
+                    });
+                });
+
+                return StSDK.jsonEncode(standard);
+            }
+        });
     };
 }(window));
